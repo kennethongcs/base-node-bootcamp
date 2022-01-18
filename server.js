41044ce010ecc05e33c47a51c180c72372f591e6
kennethongcs <kenneth.ongcs@gmail.com>
2022-01-18 16:52:03 +0800

    writeHead

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const handleIncomingRequests = (request, response) => {
   console.log('Received request');
 
   response.end('Yay!', 'utf8');
+  response.writeHead('rocketacademy', {});
 };
 
 createServer(handleIncomingRequests).listen(3004);
